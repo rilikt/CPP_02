76c313fea847d7125070fabd339962e226de9ddc
h4ns <timur.schmidt.ts@gmail.com>
2024-11-07 19:12:14 +0100

    added ex01

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,68 @@
+#include "Fixed.hpp"
+
+int Fixed::getRawBits(void) const
+{
+    // std::cout << "getRawBits member function called" << std::endl;
+    return (i);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    std::cout << "setRawBits member function called" << std::endl;
+    this->i = raw;
+}
+
+Fixed::Fixed(void)
+{
+    std::cout << "Default Constructor called" << std::endl;
+    this->i = 0;  
+}
+
+Fixed::Fixed(int value)
+{
+    std::cout << "Int Constructor called" << std::endl;
+    this->i = value << j;
+    // std::cout << i << std::endl;
+}
+
+Fixed::Fixed(float value)
+{
+    std::cout << "Float Constructor called" << std::endl; 
+    this->i = roundf(value * (1 << j)); // using round here adds more precision because regular casting always rounds down
+    // std::cout << i << std::endl;
+}
+
+float Fixed::toFloat(void) const
+{
+    return ((float)i / (1 << j));
+}
+
+int Fixed::toInt(void) const
+{
+    return (int)(i >> j);
+}
+
+std::ostream& operator<<(std::ostream &os, const Fixed &fixed)
+{
+    os << fixed.toFloat();
+    return os;
+}
+
+
+Fixed::~Fixed(void)
+{
+    std::cout << "Destructor called" << std::endl;
+} 
+
+Fixed::Fixed(const Fixed &other)
+{
+    std::cout << "Copy Constructor called" << std::endl;
+    this->i = other.getRawBits();
+}
+
+void Fixed::operator=(const Fixed &other)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    this->i = other.getRawBits();
+}
+
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -2,6 +2,7 @@
 # define FIXED_HPP
 
 #include <iostream>
+#include <cmath>
 
 class Fixed 
 {
@@ -11,13 +12,20 @@ class Fixed
     
     public:
         Fixed(void);    // Default constructor
+        Fixed(int value);    
+        Fixed(float value);
+
         Fixed(const Fixed &other);  // Copy constructor
         void operator=(const Fixed &other); // Copy assignment operator
+
+        friend std::ostream& operator<<(std::ostream &os, const Fixed &fixed);
         ~Fixed(void);   // Destructor
+
 
         int getRawBits(void) const; // const functions cant alter class variables 
         void setRawBits(int const raw);
-
+        float toFloat(void) const;
+        int toInt(void) const;
 
 };
 
